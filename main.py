baffbfb6770090de203e189b24acbeef2a2c67ab
Cariem El Wakil <c.el-wakil@anselko.de>
2022-05-25 12:30:50 +0200

    BIRD

--- main.py
+++ main.py
@@ -1,16 +1,77 @@
-# This is a sample Python script.
+import pygame
+import pygame as pg
+from pygame.locals import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+DISPLAY_HEIGHT = 1000
+DISPLAY_WIDTH = 1000
+
+HEIGHT_UNIT = DISPLAY_HEIGHT / 100
+GRAVITY = 0.1 * HEIGHT_UNIT
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+class SceneObject:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
-# Press the green button in the gutter to run the script.
+class Bird(SceneObject):
+    FLAP_LIFT = 10
+
+    def __init__(self, surface: pg.Surface, x=DISPLAY_WIDTH * 0.1, y=DISPLAY_HEIGHT * 0.5):
+        super().__init__(x, y)
+        self.score = 0
+        self.velocity = 0
+        self.surface = surface
+        self.rect = pg.Rect(self.x, self.y, DISPLAY_WIDTH * 0.05, DISPLAY_WIDTH * 0.05)
+        self.update()
+
+    def flap(self):
+        print('Bird Jump!')
+        self.velocity = self.FLAP_LIFT + HEIGHT_UNIT
+
+    def move(self):
+        self.y -= self.velocity
+        self.rect.update((self.x, self.y, self.rect.width, self.rect.height))
+
+    def draw(self):
+        pg.draw.circle(self.surface, (255, 0, 0), self.rect.center, radius=DISPLAY_WIDTH * 0.05)
+
+    def update(self):
+        print(self.rect.center)
+        self.move()
+        self.draw()
+        if self.y < DISPLAY_HEIGHT + DISPLAY_WIDTH * 0.05 or self.y < 0:
+            self.velocity -= GRAVITY
+        else:
+            self.velocity = 0
+            self.y = DISPLAY_HEIGHT - 2 * DISPLAY_WIDTH * 0.05
+
+
+def main():
+    pygame.init()  # Initialize PyGame
+
+    screen = pg.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+    surface = pg.Surface((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+
+    bird = Bird(surface)
+    fps = pg.time.Clock()
+
+    running = True
+    while running:
+        surface.fill((0, 0, 0))
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+            if event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
+                bird.flap()
+
+        bird.update()
+        screen.blit(surface, (0, 0))
+        pygame.display.update()
+        fps.tick(30)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
